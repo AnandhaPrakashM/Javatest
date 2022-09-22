d01655b37aae38cee5a126145375608fc6daa7a5
AnandhaPrakashM <imjanand5196@gmail.com>
2022-09-22 12:36:47 -0700

    new code

--- src/org/english/English.java
+++ src/org/english/English.java
@@ -5,6 +5,8 @@ import org.tamil.Tamil;
 public class English  extends Tamil{
 	public void englishLaguage() {
 		System.out.println("english");
+		System.out.println("english22");
+		System.out.println("tamil1212");
 	}
 
 }
--- src/org/tamil/Tamil.java
+++ src/org/tamil/Tamil.java
@@ -5,6 +5,9 @@ import org.telgu.Telgu;
 public class Tamil  extends Telgu{
 	public void tamilLanguage() {
 		System.out.println("tamil");
+		System.out.println("tamil1");
+		System.out.println("tamil2");
+		
 	}
 
 }
